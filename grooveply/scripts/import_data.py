71cb42b21a5cd547ae7b2d141ce2fd2c0ad1620e
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-03-24 22:44:00 +0300

    Implement import later

--- grooveply/scripts/import_data.py
+++ grooveply/scripts/import_data.py
@@ -5,9 +5,10 @@ import pandas as pd
 from tqdm import tqdm
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from apis import ApplicationAPI
 from db import create_tables
 from models import Application, ApplicationStatus, Employer
+
+from grooveply.apis.application import ApplicationAPI
 
 INPUT = "example.csv"
 
@@ -16,15 +17,8 @@ if __name__ == "__main__":
 
     df = pd.read_csv(INPUT)
 
-    for data in tqdm(df.itertuples(), total=len(df)):
-        app = Application(
-            id=data.id,
-            employer=Employer(name=data.employer),
-            status=ApplicationStatus(name=data.status),
-            description=data.description if pd.notna(data.description) else None,
-            url=data.url if pd.notna(data.url) else None,
-            created_at=data.created_at,
-            status_updated_at=data.created_at
-        )
-
-        ApplicationAPI.create(app)
+    # TODO:
+    # for data in tqdm(df.itertuples(), total=len(df)):
+    #     ApplicationAPI.create(
+            
+    #     )
