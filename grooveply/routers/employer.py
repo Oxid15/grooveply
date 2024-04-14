205df24e383da46b990bc4c4cea571663001d807
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-04-14 21:05:34 +0300

    Add simple employer page

--- grooveply/routers/employer.py
+++ grooveply/routers/employer.py
@@ -10,6 +10,7 @@ from pydantic import BaseModel
 
 from ..apis.application import ApplicationUpdateAPI
 from ..apis.automation import AutomationAPI
+from ..apis.employer import EmployerAPI
 from ..models import ApplicationStatusName, TimePeriod
 from ..settings import DB_NAME
 from ..utils import page
@@ -19,4 +20,9 @@ router = APIRouter()
 
 @router.get("/{id}", response_model=FastUI, response_model_exclude_none=True)
 def employer_page(id) -> list[AnyComponent]:
-    return page("Employer", [])
+    employer_page = EmployerAPI.get_page(id)
+    return page(employer_page.name, [
+        c.Heading(text=employer_page.name),
+        c.Paragraph(text=f"Total applications: {employer_page.total_applications}"),
+        c.Paragraph(text=f"Total locations: {employer_page.total_locations}"),
+    ])
